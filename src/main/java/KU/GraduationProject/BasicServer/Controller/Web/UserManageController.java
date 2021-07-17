ba7ace33289a0da5de3c726ca1a7b73928491e85
moon-1 <mhwgodnjs@naver.com>
2021-07-17 13:36:24 +0900

    update basic server

---
+++ src/main/java/KU/GraduationProject/BasicServer/Controller/Web/UserManageController.java
@@ -0,0 +1,68 @@
+package KU.GraduationProject.BasicServer.Controller.Web;
+
+import KU.GraduationProject.BasicServer.DataModel.User;
+import KU.GraduationProject.BasicServer.LogicProvider.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/manage/users")
+@RequiredArgsConstructor
+public class UserManageController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public String Users(Model model){
+        List<User> users = userRepository.SearchAll();
+        model.addAttribute("users",users);
+        return "manage/users";
+    }
+
+    @GetMapping("/{userId}")
+    public String User(@PathVariable Long userId,Model model){
+        User user = userRepository.SearchUser(userId);
+        model.addAttribute("user",user);
+        return "manage/user";
+    }
+
+    @GetMapping("/add")
+    public String AddUser() {
+        return "manage/addForm";
+    }
+
+    @PostMapping("/add")
+    public String AddUser(User user, RedirectAttributes redirectAttributes) {
+        User savedUser = userRepository.SaveUser(user);
+        redirectAttributes.addAttribute("userId", savedUser.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/manage/users/{userId}";
+    }
+
+
+    @GetMapping("/{userId}/edit")
+    public String EditUser(@PathVariable Long userId, Model model) {
+        User user = userRepository.SearchUser(userId);
+        model.addAttribute("user", user);
+        return "manage/editForm";
+    }
+
+    @PostMapping("/{userId}/edit")
+    public String EditUser(@PathVariable Long userId, @ModelAttribute User user) {
+        userRepository.UpdateUser(userId, user);
+        return "redirect:/manage/users/{userId}";
+    }
+
+    //Init for test
+    @PostConstruct
+    public void Init(){
+        userRepository.SaveUser(new User("userA","aaa",12));
+        userRepository.SaveUser(new User("userB","bbb",14));
+    }
+}
